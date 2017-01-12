30d814621f1b7fd28737801dbb20c7bcd0890fac
John-Paul Steed <johnpaulsteed@knights.ucf.edu>
2017-01-12 13:22:27 -0500

    Added logger

--- src/entity/SRMREntity.java
+++ src/entity/SRMREntity.java
@@ -1,5 +1,12 @@
 package entity;
 
 public class SRMREntity extends SimulationEntity {
+	
+	
+
+	public SRMREntity(int gridX, int gridY) {
+		super(gridX, gridY);
+		// TODO Auto-generated constructor stub
+	}
 
 }
