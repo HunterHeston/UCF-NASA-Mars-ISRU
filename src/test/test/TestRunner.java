bab5488d1560d727f2d957f99a9a7666a2be460d
Aforan <aforan@knights.ucf.edu>
2017-01-12 23:15:11 -0500

    Simple UI to print the EnvironmentGrid

--- src/test/test/TestRunner.java
+++ src/test/test/TestRunner.java
@@ -1,41 +1,124 @@
 package test.test;
 
-import org.junit.Test;
-import federate.SimulationEntityExecution;
+import environment.EnvironmentGrid;
+import environment.EnvironmentGridFactory;
+import environment.GridCell;
 
-import java.util.Queue;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Andrew on 1/12/2017.
  */
-public abstract class TestRunner {
+public class TestRunner extends JPanel implements Runnable {
 
-    public static abstract class TestCase<T extends SimulationEntityExecution> {
-        public TestCase() {}
-        public abstract boolean doAction(T entity);
-        public abstract boolean isComplete(T entity);
-    }
+    public GridCell[][] grid;
 
-    public abstract Queue<TestCase> getActionQueue();
-    public abstract SimulationEntityExecution getEntity();
+    public void drawGrid(Graphics g) {
+        g.setColor(Color.GREEN);
+        g.drawRect(6, 6, 500, 500);
 
-    @Test
-    public void runTests() {
-        Queue<TestCase> testQueue = this.getActionQueue();
-        SimulationEntityExecution entity = this.getEntity();
+        int cellSize = 500 / grid.length;
 
-        assert testQueue != null && !testQueue.isEmpty();
-        assert entity != null;
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid.length; j++) {
+                g.setColor(Color.GREEN);
+                g.drawRect((j*cellSize) + 6, (i*cellSize) + 6, cellSize, cellSize);
 
-        while(!testQueue.isEmpty()) {
-            TestCase testCase = testQueue.peek();
-
-            testCase.doAction(entity);
-            entity.doAction();
-
-            if(testCase.isComplete(entity)) {
-                testQueue.poll();
+                g.setColor(Color.white);
+                //g.drawChars(("["+j+","+i+"]").toCharArray(), 0, 10, ((j+1)*cellSize) + 6, ((i+1)*cellSize) + 6);
             }
         }
     }
+
+    public void drawEntities(Graphics g) {
+        int cellSize = 500 / grid.length;
+
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                GridCell cell = grid[i][j];
+                if(cell.isBlocked()) {
+                    g.setColor(Color.gray);
+                    g.fillRect((j*cellSize) + 6, (i*cellSize) + 6, cellSize, cellSize);
+                } else if (cell.isOccupied()) {
+                    g.setColor(Color.white);
+                    g.drawChars(new char[] {'E'}, 0, 1, ((j+1)*cellSize)-(cellSize/2), ((i+1)*cellSize));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                update();
+                Thread.sleep(200);
+            } catch(Exception e) {
+
+            }
+        }
+    }
+
+    public void clearScreen(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
+
+    public void update() {
+        Graphics g = getGraphics();
+        clearScreen(g);
+
+        drawGrid(g);
+        drawEntities(g);
+
+        paintComponents(g);
+    }
+
+    public TestRunner(GridCell[][] grid) {
+        this.grid = grid;
+    }
+
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("UCF - Mars Sim Test Runner");
+        frame.setSize(new Dimension(530, 600));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String[] gridSource = {
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+                "B B B B B B B B B _ _ _ _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ B B B B B B _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ B B B B B B B B B B _ _ _ _ ",
+                "_ _ _ _ _ _ B _ _ _ B B B B _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ B _ _ B _ _ _ _ _ B _ _ _ _ ",
+                "_ _ _ _ _ _ B _ _ B _ B _ _ _ B _ _ _ _ ",
+                "_ _ _ _ _ _ B _ _ B _ B _ _ B B B B B B ",
+                "_ _ _ _ _ _ B _ _ B _ B _ _ B _ _ _ _ _ ",
+                "_ _ _ _ _ _ B _ _ B _ B _ _ B _ _ _ _ _ ",
+                "_ _ _ _ _ _ B _ _ B _ B _ _ B _ _ _ _ _ ",
+                "_ _ _ _ _ _ B _ _ B _ B _ _ B B B B B _ ",
+                "_ _ _ _ _ _ B B B B _ B _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+                "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ",
+        };
+
+        EnvironmentGrid grid = EnvironmentGridFactory.gridFromTXT(gridSource);
+
+        grid.placeEntity(1, 0, 0, 1);
+        grid.placeEntity(2, 4, 4, 1);
+        grid.placeEntity(3, 6, 4, 1);
+
+        TestRunner runner = new TestRunner(grid.gridArray);
+        frame.getContentPane().add(runner);
+
+        frame.setVisible(true);
+        Thread thread = new Thread(runner);
+        thread.start();
+    }
 }
+
