08b73ae47375b71eaa74dd7b41d3e61dc63ec934
John-Paul Steed <johnpaulsteed@knights.ucf.edu>
2017-01-14 14:31:06 -0500

    Updated SRMR

--- src/entity/SRMREntityState.java
+++ src/entity/SRMREntityState.java
@@ -3,10 +3,11 @@ package entity;
 import org.apache.log4j.Logger;
 
 import environment.RegolithData;
+import state.SimpleChargeableEntityState;
 
-public class SRMREntity extends SimulationEntity {
-	
-	final static Logger logger = Logger.getLogger(SRMREntity.class);
+public class SRMREntityState extends SimpleChargeableEntityState {
+
+	final static Logger logger = Logger.getLogger(SRMREntityState.class);
 	
 	public enum SRMRState {
 		Standby,
@@ -24,22 +25,20 @@ public class SRMREntity extends SimulationEntity {
 	
 	public final static int EMPTY = 0;
 	
-	private int ISRU_X;
-	private int ISRU_Y;
-	private double MAX_CAPACITY;
-	private double MAX_CHARGE;
-	
+	private double maxPayload;
 	private RegolithData payload;
-	private int dumpSiteLocationX;
-	private int dumpSiteLocationY;
 	
-	private double charge;
+	private GridIndex dumpSiteIndex;
 	
 	private SRMRState userState = SRMRState.Standby;
-
-	public SRMREntity(int gridX, int gridY) {
-		super(gridX, gridY);
-		// TODO Auto-generated constructor stub
+	
+	public SRMREntityState(long identifier, int gridX, int gridY, 
+							int isruGridX, int isruGridY, double capacity,
+							double movementSpeed, double gridCellSize,
+							double maxPayload) {
+		
+		super(identifier, gridX, gridY, isruGridX, isruGridY, capacity, movementSpeed, gridCellSize);
+		this.maxPayload = maxPayload;
 	}
 
 	public void setUserState(SRMRState userState) {
@@ -48,35 +47,42 @@ public class SRMREntity extends SimulationEntity {
 	
 	public void beginMoveToISRU(){
 		assert userState == SRMRState.Standby;
-		// begin movement
 		
-		setUserState(SRMRState.MoveToISRU);
-		logger.debug("SRMR State transition: Standby -> MoveToISRU");
+		if(this.needsCharge()){
+			this.handleCharge();
+			setUserState(SRMRState.HandleCharge);
+			logger.debug("SRMR State transition: Standby -> HandleCharge");
+		}else{
+			this.beginPathFinding(this.isruIndex.row, this.isruIndex.col);
+			setUserState(SRMRState.MoveToISRU);
+			logger.debug("SRMR State transition: Standby -> MoveToISRU");
+		}
+		
 	}
 	
 	public void ISRUArrivalResponse(){
 		assert userState == SRMRState.MoveToISRU;
 		assert this.movementState == MovementState.Stopped;
-		assert this.gridIndex.equals(new GridIndex(ISRU_X+1, ISRU_Y)) ||
-				this.gridIndex.equals(new GridIndex(ISRU_X-1, ISRU_Y)) ||
-				this.gridIndex.equals(new GridIndex(ISRU_X, ISRU_Y+1)) ||
-				this.gridIndex.equals(new GridIndex(ISRU_X, ISRU_Y-1));
+		assert adjacentToISRU(this.gridIndex, this.isruIndex);
 		
 		setUserState(SRMRState.AtISRU);
 		logger.debug("SRMR State transition: MoveToISRU -> AtISRU");
 	}
 	
-	public void handleCharge(){
+	public void chargeRover(){
 		assert userState == SRMRState.AtISRU;
-		assert charge != MAX_CHARGE;
+		assert this.chargeState == ChargeState.Null;
 		
 		setUserState(SRMRState.HandleCharge);
 		logger.debug("SRMR State transition: AtISRU -> HandleCharge");
+		
+		this.chargeState = ChargeState.TransitToISRU;
+		this.attemptConnectToISRU();
 	}
 	
 	public void handleChargeResponse(){
 		assert userState == SRMRState.HandleCharge;
-		assert charge == MAX_CHARGE;
+		assert this.chargeState == ChargeState.Null;
 		
 		setUserState(SRMRState.AtISRU);
 		logger.debug("SRMR State transition: HandleCharge -> AtISRU");
@@ -84,15 +90,21 @@ public class SRMREntity extends SimulationEntity {
 	
 	public void beginLoadRegolith(){
 		assert userState == SRMRState.AtISRU;
-		assert payload.quantity < MAX_CAPACITY;
+		assert payload.quantity >= EMPTY && payload.quantity <= maxPayload;
 		
-		setUserState(SRMRState.LoadRegolith);
-		logger.debug("SRMR State transition: AtISRU -> LoadRegolith");
+		if(payload.quantity < maxPayload){
+			setUserState(SRMRState.LoadRegolith);
+			logger.debug("SRMR State transition: AtISRU -> LoadRegolith");
+		}else{
+			setUserState(SRMRState.FindDumpSite);
+			logger.debug("SRMR State transition: AtISRU -> FindDumpSite");
+		}
+		
 	}
 	
 	public void LoadRegolithResponse(RegolithData payload){
 		assert userState == SRMRState.LoadRegolith;
-		assert payload.quantity >= EMPTY && payload.quantity <= MAX_CAPACITY;
+		assert payload.quantity >= EMPTY && payload.quantity <= maxPayload;
 		
 		this.payload = payload;
 		if(payload.quantity == EMPTY){
@@ -114,8 +126,9 @@ public class SRMREntity extends SimulationEntity {
 	public void selectDumpSiteResponse(int locationX, int locationY){
 		assert userState == SRMRState.WaitForDumpSite;
 		
-		dumpSiteLocationX = locationX;
-		dumpSiteLocationY = locationY;
+		dumpSiteIndex.row = locationX;
+		dumpSiteIndex.col = locationY;
+		this.beginPathFinding(dumpSiteIndex.row, dumpSiteIndex.col);
 		setUserState(SRMRState.MoveToDumpSite);
 		logger.debug("SRMR State transition: WaitForDumpSite -> MoveToDumpSite");
 	}
@@ -123,8 +136,8 @@ public class SRMREntity extends SimulationEntity {
 	public void arriveAtDumpSite(){
 		assert userState == SRMRState.MoveToDumpSite;
 		assert this.movementState == MovementState.Stopped;
-		assert this.gridIndex.row == dumpSiteLocationX &&
-				this.gridIndex.col == dumpSiteLocationY;
+		assert this.gridIndex.row == dumpSiteIndex.row &&
+				this.gridIndex.col == dumpSiteIndex.col;
 		
 		setUserState(SRMRState.AtDumpSite);
 		logger.debug("SRMR State transition: MoveToDumpSite -> AtDumpSite");
@@ -140,7 +153,7 @@ public class SRMREntity extends SimulationEntity {
 	
 	public void dumpRegolithResponse(RegolithData payload){
 		assert userState == SRMRState.DumpRegolith;
-		assert payload.quantity >= EMPTY && payload.quantity <= MAX_CAPACITY;
+		assert payload.quantity >= EMPTY && payload.quantity <= maxPayload;
 		
 		this.payload = payload;
 		if(payload.quantity == EMPTY){
