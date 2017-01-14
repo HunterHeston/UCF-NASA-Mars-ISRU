4f2044859a389b80259bd6f28f83a9e881db1900
Hunter <Hunter_Heston@yahoo.com>
2017-01-14 10:28:25 -0500

    Added test and rassor classes

--- src/entity/RASSOREntity.java
+++ src/entity/RASSOREntity.java
@@ -1,12 +1,31 @@
 package entity;
 
+
+import org.apache.log4j.L
+
+
+
 /**
  * Created by Hunter on 1/12/2017.
  */
 public class RASSOREntity {
 
+    public enum States{
 
-    //change    
+        Standby,
+        MineFinding,
+        MineSelected,
+        HandleCharge,
+        MovingToMine,
+        AtMine,
+        Mining,
+        MiningComplete,
+        MovingToISRU,
+        AtISRU,
+        DepositingRegolith,
+    }
+
+
 
 
 }
---
+++ src/test/entity/RASSOREntityTest.java
@@ -0,0 +1,10 @@
+package test.entity;
+
+/**
+ * Created by Hunter on 1/12/2017.
+ */
+public class RASSOREntityTest {
+
+
+
+}
